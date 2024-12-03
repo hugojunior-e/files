c917eae3bf8a05c0990823579df2f9e4da528517
Hugo Junior <82850382+hugojunior-e@users.noreply.github.com>
2024-12-03 10:37:34 -0300

    Update api.js

--- api.js
+++ api.js
@@ -95,10 +95,18 @@ const diasDaSemana = [
 ];
 
 const numerosPorExtenso = [
-    "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
-    "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove",
-    "vinte", "vinte e um", "vinte e dois", "vinte e três", "vinte e quatro", "vinte e cinco",
-    "vinte e seis", "vinte e sete", "vinte e oito", "vinte e nove", "trinta", "trinta e um"
+    "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", 
+    "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", 
+    "dezoito", "dezenove", "vinte", "vinte e um", "vinte e dois", "vinte e três", 
+    "vinte e quatro", "vinte e cinco", "vinte e seis", "vinte e sete", "vinte e oito", 
+    "vinte e nove", "trinta", "trinta e um", "trinta e dois", "trinta e três", 
+    "trinta e quatro", "trinta e cinco", "trinta e seis", "trinta e sete", 
+    "trinta e oito", "trinta e nove", "quarenta", "quarenta e um", "quarenta e dois", 
+    "quarenta e três", "quarenta e quatro", "quarenta e cinco", "quarenta e seis", 
+    "quarenta e sete", "quarenta e oito", "quarenta e nove", "cinquenta", 
+    "cinquenta e um", "cinquenta e dois", "cinquenta e três", "cinquenta e quatro", 
+    "cinquenta e cinco", "cinquenta e seis", "cinquenta e sete", "cinquenta e oito", 
+    "cinquenta e nove"
 ];
 
 function numeroPorExtenso(num) {
