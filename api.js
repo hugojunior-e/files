774d7e18ce8df38246a94febadfec7a4cc751729
Hugo Junior <82850382+hugojunior-e@users.noreply.github.com>
2024-12-03 07:55:15 -0300

    Update api.js

--- api.js
+++ api.js
@@ -5,24 +5,30 @@ const url_atas = 'https://parseapi.back4app.com/classes/atas';
 
 
 
-async function apiQueryData( p_url, populateTable ) {
-    try {
-        const response = await fetch(p_url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Parse-REST-API-Key': apiKey,
-                'X-Parse-Application-Id': appId
-            }
-        });
+async function apiQueryData( p_url, populateTable, paramsQuery ) {
+    new_url = p_url;
 
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status}`);
-        }
-
-        const data = await response.json();
-        populateTable(data);
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error);
+    if ( paramsQuery !== undefined ) {
+        const query = {
+            where: JSON.stringify(paramsQuery) /
+        };
+    
+        new_url = `${p_url}?${new URLSearchParams(query)}`;
     }
+    
+    const response = await fetch(new_url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Parse-REST-API-Key': apiKey,
+            'X-Parse-Application-Id': appId
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`Erro: ${response.status}`);
+    }
+
+    const data = await response.json();
+    populateTable(data);
 }
